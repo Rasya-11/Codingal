52fead5090de9d7d66e150d62a6944c2e538680f
Rasya-11 <rasyarao26@gmail.com>
2025-12-04 14:30:01 +0000

    Homework

---
+++ Module4/Lesson24/Script2.js
@@ -0,0 +1,6 @@
+function Myfunction1(){
+    var a = 4
+    document.getElementById("demo").innerHTML = a*a;
+}
+ var text = document.getElementById("1").innerHTML;
+            alert("The first heading is " + text)
