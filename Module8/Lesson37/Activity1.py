0ca9b0112d5dc7c8d6061e193e3603c0138b124d
Rasya-11 <rasyarao26@gmail.com>
2026-03-05 17:14:38 +0000

    Python print statement

---
+++ Module8/Lesson37/Activity1.py
@@ -0,0 +1,8 @@
+print("hello")
+age = 14
+name = input("What is your name?")
+hobby = input("What is your hobby?")
+# print("My name is", name, ". I am", age, "years old.")
+print(f"My name is {name}. I am {age} years old.")
+print("My hobby is " + hobby)
+# print("My hobby is", hobby)
---
+++ Module8/Lesson37/Activity2.py
@@ -0,0 +1,10 @@
+# Print a sentence and a number using Python Print Statement. Also, print all the keywords present in Python.
+print("Welcome to the world of programming!")
+a = 5
+b = 6
+print("sum is", a + b)
+
+import keyword
+
+print("Keywords in python are")
+print(keyword.kwlist)
