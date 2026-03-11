12eefe5138126a139b599210fec04b60eb67676d
Rasya-11 <rasyarao26@gmail.com>
2026-03-11 17:44:10 +0000

    Homework

--- Module8/Lesson38/Homework.py
+++ Module8/Lesson38/Homework.py
@@ -1,2 +1,13 @@
 #Raj scored 40, 70, 50 and 60 out of 100 in maths, science, Hindi and English. 
 # Find the percentage he got.
+
+maths = 40
+science = 70
+hindi = 50
+english = 60
+
+total_marks = maths + science + hindi + english
+print("Total Marks:", total_marks)
+
+percentage = total_marks / 400 * 100
+print("Percentage:", percentage)
---
+++ Module8/Lesson38/Homework2.py
@@ -0,0 +1,16 @@
+x = 5
+y = 9
+z = 1
+
+# Arrange numbers in ascending order
+numbers = [x, y, z]
+numbers.sort()
+
+# Assign sorted values back
+x = numbers[0]
+y = numbers[1]
+z = numbers[2]
+
+print("x =", x)
+print("y =", y)
+print("z =", z)
