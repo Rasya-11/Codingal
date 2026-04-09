5138a44c628d23d1e9699c9810827e7a7f48867e
Rasya-11 <rasyarao26@gmail.com>
2026-04-09 15:46:06 +0100

    Homework

--- Module9/Lesson43/CountNumber.py
+++ Module9/Lesson43/CountNumber.py
@@ -3,3 +3,16 @@
 
 
 # if len(i)>1 and i[0]==i[-1]:
+
+def count_strings(str_list):
+    count = 0
+
+    for i in str_list:
+        if len(i) > 1 and i[0] == i[-1]:
+            count += 1
+
+    return count
+words = ["abc", "aba", "1221", "xyx", "a", "bb", "xyz"]
+
+print("Original list:", words)
+print("Count of matching strings:", count_strings(words))
--- Module9/Lesson43/SumAndAVerageList.py
+++ Module9/Lesson43/SumAndAVerageList.py
@@ -9,3 +9,22 @@
 # avg=sum/len(List)
 
 #Sort the list first and smallest number = 1st item (index 0), largest number = last item
+
+List = [10, 20, 30, 40, 15]
+
+total = 0
+for i in List:
+    total = total + i
+
+avg = total / len(List)
+
+List.sort()
+
+smallest = List[0]
+largest = List[-1]
+
+print("List:", List)
+print("Sum:", total)
+print("Average:", avg)
+print("Smallest number:", smallest)
+print("Largest number:", largest)
