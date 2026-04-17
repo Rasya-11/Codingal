93d4e4dcfe13279e6732afaafc5ae4a14e4371e6
Rasya-11 <rasyarao26@gmail.com>
2026-04-17 16:46:39 +0100

    Homework

--- Module9/Lesson44/User_Input.py
+++ Module9/Lesson44/User_Input.py
@@ -5,3 +5,15 @@
 
 
 # convert a list to a tuple-tuple()
+
+# Set of numbers
+my_set = {10, 20, 30}
+
+# Take input from user
+user_input = int(input("Enter a number: "))
+
+# Check if input is in set
+if user_input in my_set:
+    print("The number is in the set.")
+else:
+    print("The number is not in the set.")
