28986dee8118adbe23b9c271a5ca80d4bdd06bcd
Rasya-11 <rasyarao26@gmail.com>
2026-04-23 06:36:10 +0100

    Homework

---
+++ Module9/Lesson46/Homework.py
@@ -0,0 +1,15 @@
+class Expression:
+    def __init__(self, num1, num2, num3):
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+
+    def add_numbers(self):
+        result = self.num1 + self.num2 + self.num3
+        print("The sum is:", result)
+
+exp1 = Expression(10, 20, 30)
+exp2 = Expression(5, 15, 25)
+
+exp1.add_numbers()
+exp2.add_numbers()
